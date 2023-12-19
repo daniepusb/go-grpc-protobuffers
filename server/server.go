7f2fde34718afbe561e9292c29a74a619f5a973b
Daniel <danie.p.usb@gmail.com>
2023-12-19 03:09:42 +0100

    feat: create GetStudent and SetStudent

--- server/server.go
+++ server/server.go
@@ -19,3 +19,32 @@ func NewStudentServer(repo repository.Repository) *server {
 		repo: repo,
 	}
 }
+
+
+
+func (s *Server) GetStudent(ctx context.Context, req *studentpb.GetStudentRequest) (*studentpb.Student, error) {
+	student, err := s.repo.GetStudent(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &studentpb.Student{
+		Id:   student.Id,
+		Name: student.Name,
+		Age:  student.Age,
+	}, nil
+}
+
+func (s *Server) SetStudent(ctx context.Context, req *studentpb.Student) (*studentpb.SetStudentResponse, error) {
+	student := &models.Student{
+		Id:   req.GetId(),
+		Name: req.GetName(),
+		Age:  req.GetAge(),
+	}
+	err := s.repo.SetStudent(ctx, student)
+	if err != nil {
+		return nil, err
+	}
+	return &studentpb.SetStudentResponse{
+		Id: student.Id,
+	}, nil
+}
